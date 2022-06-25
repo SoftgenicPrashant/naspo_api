8d8112eb095ad415bce3a26ac4d386f5c1503edb
SoftgenicPrashant <softgenics.prashant@gmail.com>
2022-06-26 02:26:49 +0530

    udated

--- app.js
+++ app.js
@@ -50,12 +50,4 @@ app.get("/get",(req,res)=>{
 res.json({"naspodata":naspodata});
 })
 
-app.listen(PORT, (error) => {
-  if (!error) {
-    console.log(
-      "Server is Successfully Running, and App is listening on port " + PORT
-    );
-  } else {
-    console.log("Error occurred, server can't start", error);
-  }
-});
+app.listen();
